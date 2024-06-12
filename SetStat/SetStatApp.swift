f0d63ce8e1198110330cdc61704c125e90f431ae
MilanLabuss <milanlabus29@gmail.com>
2024-06-12 16:20:12 +0100

    Initial Commit

---
+++ SetStat/SetStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetStatApp.swift
+//  SetStat
+//
+//  Created by Milan Labus on 12/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SetStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
