06a42d7e9331876d0e8a62a02cae85b7be7cece8
MilanLabuss <milanlabus29@gmail.com>
2024-06-14 17:50:22 +0100

    All Code Set up ready to start writting the add to db functions

---
+++ SetStat/View/EditExeriseView.swift
@@ -0,0 +1,18 @@
+//
+//  EditExeriseView.swift
+//  SetStat
+//
+//  Created by Milan Labus on 13/06/2024.
+//
+
+import SwiftUI
+
+struct EditExeriseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditExeriseView()
+}
