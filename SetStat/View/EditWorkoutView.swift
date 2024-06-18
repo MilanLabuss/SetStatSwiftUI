5fd2d3f462544c3488f1e30b5b58ef003ab72258
MilanLabuss <milanlabus29@gmail.com>
2024-06-18 15:51:22 +0100

    Added full functionality now i need to List all workouts in Order of Date

---
+++ SetStat/View/EditWorkoutView.swift
@@ -0,0 +1,18 @@
+//
+//  EditWorkoutView.swift
+//  SetStat
+//
+//  Created by Milan Labus on 18/06/2024.
+//
+
+import SwiftUI
+
+struct EditWorkoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditWorkoutView()
+}
