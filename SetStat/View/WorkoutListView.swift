3c0ac0c09a6f4a8ac90a25faaaab3f1d264a600e
MilanLabuss <milanlabus29@gmail.com>
2024-07-05 15:38:38 +0100

    Everything working as it should but trying to extract the List Breaks the Entire Code

---
+++ SetStat/View/WorkoutListView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutListView.swift
+//  SetStat
+//
+//  Created by Milan Labus on 05/07/2024.
+//
+
+import SwiftUI
+
+struct WorkoutListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutListView()
+}
