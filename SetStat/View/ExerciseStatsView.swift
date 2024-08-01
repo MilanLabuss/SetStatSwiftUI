93eff0a8b5a0b8d37a9641286f0f35e24d25fb54
MilanLabuss <milanlabus29@gmail.com>
2024-08-01 10:56:39 +0100

    Chart displaying correct Data for the MaxWeight next to implement 1 rep max calculations

---
+++ SetStat/View/ExerciseStatsView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseStatsView.swift
+//  SetStat
+//
+//  Created by Milan Labus on 30/07/2024.
+//
+
+import SwiftUI
+
+struct ExerciseStatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseStatsView()
+}
