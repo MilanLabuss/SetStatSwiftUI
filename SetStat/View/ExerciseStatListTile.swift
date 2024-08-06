b88dfb8f1090c7e36a5cefed12876d2903b359d2
MilanLabuss <milanlabus29@gmail.com>
2024-08-06 11:33:45 +0100

    Statistics Complete

---
+++ SetStat/View/ExerciseStatListTile.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseStatListTile.swift
+//  SetStat
+//
+//  Created by Milan Labus on 01/08/2024.
+//
+
+import SwiftUI
+
+struct ExerciseStatListTile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseStatListTile()
+}
