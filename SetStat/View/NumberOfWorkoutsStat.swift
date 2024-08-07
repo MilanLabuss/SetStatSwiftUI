3fd36385e2763644cc4df17112384df76bef6576
MilanLabuss <milanlabus29@gmail.com>
2024-08-07 20:13:21 +0100

    Used Anchor to fix formattting issues on X axis of Charts

---
+++ SetStat/View/NumberOfWorkoutsStat.swift
@@ -0,0 +1,18 @@
+//
+//  NumberOfWorkoutsStat.swift
+//  SetStat
+//
+//  Created by Milan Labus on 07/08/2024.
+//
+
+import SwiftUI
+
+struct NumberOfWorkoutsStat: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NumberOfWorkoutsStat()
+}
