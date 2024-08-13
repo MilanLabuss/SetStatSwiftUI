eb76f3e64756cc4d9a357b7d07aa819b4608a623
MilanLabuss <milanlabus29@gmail.com>
2024-08-13 14:46:38 +0100

    Add functionality of creating a set, deleting it and copying it

---
+++ SetStat/View/SetEditor.swift
@@ -0,0 +1,18 @@
+//
+//  SetEditor.swift
+//  SetStat
+//
+//  Created by Milan Labus on 12/08/2024.
+//
+
+import SwiftUI
+
+struct SetEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetEditor()
+}
---
+++ SetStat/View/SetsView.swift
@@ -0,0 +1,18 @@
+//
+//  SetsView.swift
+//  SetStat
+//
+//  Created by Milan Labus on 12/08/2024.
+//
+
+import SwiftUI
+
+struct SetsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetsView()
+}
