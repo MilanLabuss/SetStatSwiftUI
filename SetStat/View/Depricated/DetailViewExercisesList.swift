20573e145d85c5d6ce312c5cc56394b13f563573
MilanLabuss <milanlabus29@gmail.com>
2024-08-14 18:42:33 +0100

    Everything working as indended the only thing remaining is to try and make weight and reps optional in the Models

---
+++ SetStat/View/Depricated/DetailViewExercisesList.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewExercisesList.swift
+//  SetStat
+//
+//  Created by Milan Labus on 14/08/2024.
+//
+
+import SwiftUI
+
+struct DetailViewExercisesList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailViewExercisesList()
+}
